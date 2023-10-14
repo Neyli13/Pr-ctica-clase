64af937669dd0b43bbf54fcc10da6a35ee5cc3c4
Neyli13 <93058901+Neyli13@users.noreply.github.com>
2023-10-14 16:57:10 -0600

    Add files via upload

---
+++ practica clase mysql.sql
@@ -0,0 +1,23 @@
+use sakila;  
+CREATE TABLE Alumnos 
+( 
+IdAlumno int  primary key not null, 
+Nombre varchar (25), 
+Apellido Varchar (25), 
+Edad int, 
+Direccion_Residencia varchar (50) 
+);
+
+INSERT INTO Alumnos (IdAlumno, Nombre, Apellido, Edad, 
+Direccion_Residencia)  
+VALUES  
+('0102', 'Franklin1', 'Garcia', '25', 'avenida 01'),  
+('0103', 'Franklin2', 'Garcia', '25', 'avenida 02'),   
+('0104', 'Franklin3', 'Garcia', '25', 'avenida 03');  
+
+
+SELECT * FROM alumnos; 
+
+SELECT nombres, apellidos FROM alumnos; 
+
+SELECT * FROM alumnos where Nombres='Franklin1'; 
